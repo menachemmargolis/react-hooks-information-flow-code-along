1b72b4c32c339f31952632a190acce26e6afb80c
menachem margolis <margolismendel5728@gmail.com>
2021-02-23 18:15:05 -0500

    commit tree 9f86f8bfe1c94e13add0333788a1cda237f41763

--- src/Parent.js
+++ src/Parent.js
@@ -3,13 +3,21 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
+  const [childrenColor, setChildrenColor] = useState("#FFF");
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newRandomColor)
+  }
+
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
